1437ec618f1c253dc4641d42d036a1f17cd53428
dollyanusha <dollyanusha@dal.ca>
2019-09-22 05:01:17 -0700

    saving ex:9.7

---
+++ Exercise_9_7.py
@@ -0,0 +1,31 @@
+myfile = open(“words.txt”,’r’)
+
+word = word.strip()
+
+def is_pair(word):
+
+	for letter in word:
+
+		i = 0
+
+		count = 0
+
+		while i<len(word)-1
+
+			if word[I]==word[i+1]
+
+				count = count+1
+
+				if count == 3:
+
+					print(word)
+
+				i= i+2
+
+			else:
+
+				count=0
+
+				i= i+1
+
+is_word(word)
