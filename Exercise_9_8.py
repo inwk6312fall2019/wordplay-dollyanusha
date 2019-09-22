9bf02a7b122c154261b77a2ad4cb6cd2f21526ef
dollyanusha <dollyanusha@dal.ca>
2019-09-22 05:04:05 -0700

    saving ex:9.8

---
+++ Exercise_9_8.py
@@ -0,0 +1,23 @@
+def is_palindrome(num,start):
+
+	num=str(num)
+
+	i=strat
+
+	j=len(num)-1
+
+	count=0
+
+	while i<=j:
+
+		if numstring[I]==numstring[j]:
+
+			print(num)
+
+		else:
+
+			return False
+
+		i= i+1
+
+		j=j+1
